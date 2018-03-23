2605dfe99938a1d64179f303651b6d33cb074fae
Guest User <Guest@epicodus-21.local>
2018-03-23 12:20:22 -0700

    add business logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,44 @@
 //business logic
+function trackRecommender(confidence, end, pace, flow, size){
+  var result1 = ruby;
+  var result2 = c#;
+  var result3 = java;
+  var result4 = php;
+
+if (confidence == 3 || 4){
+  if (end == front){
+    return result1;
+  } else {
+    return result4;
+  }
+else {
+  if (pace == 2) && (size == 2){
+    return result3;
+  } else {
+    return result2;
+  }
+}
+}
+
+if (pace == 1 || 2){
+  if (flow == consistent){
+    if (size == 1 || 2){
+      return result2;
+    } else {
+      if (size == 3 || 2){
+        return result4;
+      } else {
+        return result3;
+      }
+    }
+  }
+} else {
+  if (size == 3){
+    return result1;
+  } else {
+    return result2;
+  }
+}
 
 
 
